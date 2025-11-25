5c8b1b359b295b147d7c125e2b8aca1a0cb11290
rochellesuarezuchi <uchirochelle@minsu.edu.ph>
2025-11-25 16:34:25 +0800

    m

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-// Set CORS headers
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN, *");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 86400");  // 24 hours
-
 // Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
